3ec256955843aaade21811d35dd833a352256c1e
Shyamala9926 <shyamlajanamoni2002@gmail.com>
2024-12-02 23:30:32 +0530

    new changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,20 @@
+// 
+
 import React from "react";
-import './Navbar.css'
-import menu_icon from '../../assets/menu.png'
-import logo from '../../assets/logo.png'
-import search_icon from '../../assets/search.png'
-import upload_icon from '../../assets/upload.png'
-import more_icon from '../../assets/more.png'
-import notification_icon from '../../assets/notification.png'
-import profile_icon from '../../assets/jack.png'
+import './Navbar.css';
+import menu_icon from '../../assets/menu.png';
+import logo from '../../assets/logo.png';
+import search_icon from '../../assets/search.png';
+import upload_icon from '../../assets/upload.png';
+import more_icon from '../../assets/more.png';
+import notification_icon from '../../assets/notification.png';
+import profile_icon from '../../assets/jack.png';
 import { Link } from "react-router-dom";
 
-const Navbar =({setSidebar})=>{
-    return(
-        <nav className="flex-dev">
-            {/* <div className="nav-lef flex-div">
-                <img className="menu-icon" onClick={()=>setSidebar(prev=>prev===false?true:false)} src={menu_icon}alt=""/>
-                <img className="logo" src={logo}alt=""/>
-            </div> */}
-
-<div className="nav-left flex-div">
+const Navbar = ({ setSidebar }) => {
+    return (
+        <nav className="navbar">
+            <div className="nav-left">
                 {/* Menu icon toggles the sidebar */}
                 <img
                     className="menu-icon"
@@ -25,25 +22,26 @@ const Navbar =({setSidebar})=>{
                     src={menu_icon}
                     alt="Menu Icon"
                 />
-               <Link to='/'> <img className="logo" src={logo} alt="Logo" /></Link>
+                <Link to="/">
+                    <img className="logo" src={logo} alt="Logo" />
+                </Link>
             </div>
-            
-            <div className="nav-middle flex-div">
-            <div className="search-box flex-div">
-            <input type="text" placeholder="search"/>
-            <img src ={search_icon} alt="" />
-            </div>
-                </div>
 
-                <div className="nav-right flex-dev">
-                    <img src={upload_icon}alt="" />
-                    <img src={more_icon} alt="" />
-                    <img src={notification_icon}alt="" />
-                    <img src={profile_icon} className="user-icon" alt="" />
+            <div className="nav-middle">
+                <div className="search-box">
+                    <input type="text" placeholder="Search" />
+                    <img src={search_icon} alt="Search Icon" />
                 </div>
+            </div>
+
+            <div className="nav-right">
+                <img src={upload_icon} alt="Upload Icon" />
+                <img src={more_icon} alt="More Icon" />
+                <img src={notification_icon} alt="Notification Icon" />
+                <img src={profile_icon} className="user-icon" alt="Profile Icon" />
+            </div>
         </nav>
-    )
-}
-
+    );
+};
 
 export default Navbar;
